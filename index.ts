0f2fbf3aa760aa9e3860d3cd4a31e453ed2e6ecb
ArtiolaDomiCaka <artioladomi@hotmail.com>
2022-03-20 22:00:09 +0100

    .

--- index.ts
+++ index.ts
@@ -1,90 +1,119 @@
-import express, { request }  from "express";
-import cors from 'cors'
+import express, { request } from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import 'dotenv/config'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const prisma = new PrismaClient({ log: ['error', 'info', 'query', 'warn']})
+const prisma = new PrismaClient({ log: ["error", "info", "query", "warn"] });
 
-function createToken (id: number){
-//@ts-ignore
-const token = jwt.sign({id: id}, process.env.my_secret, {expiresIn: '3days'})
-return token
+function createToken(id: number) {
+  //@ts-ignore
+  const token = jwt.sign({ id: id }, process.env.my_secret, {
+    expiresIn: "3days",
+  });
+  return token;
 }
 
- async function getUserFromToken(token: string){
-     //@ts-ignore
-    const data = jwt.verify(token, process.env.my_secret)
-    //@ts-ignore
-    const user = await prisma.user.findUnique({where: {id: data.id}, include: {orders: {include: {item: true}}}})
-    return user
+async function getUserFromToken(token: string) {
+  //@ts-ignore
+  const data = jwt.verify(token, process.env.my_secret);
+  //@ts-ignore
+  const user = await prisma.user.findUnique({
+      //@ts-ignore
+    where: { id: data.id },
+    include: { orders: { include: { item: true } } },
+  });
+  return user;
 }
 //SIGN UP
-app.post('/sign-up', async(req, res )=>{
-    const {name, email, password} = req.body
+app.post("/sign-up", async (req, res) => {
+  const { name, email, password } = req.body;
 
-    try{
-        const hash = bcrypt.hashSync(password)
-        const user = await prisma.user.create({ data: { name, email, password: hash}})
-        res.send({user, token: createToken(user.id)})
-    }catch(err){
-
-     //@ts-ignore
-     res.status(400).send({error: err.message })
-    }
-
-})
-
-
-
-//SIGN IN 
-app.post('/sign-in', async(req, res) =>{
-    const { email, password} =req.body
-    try{
-        const user =  await prisma.user.findUnique({ where: {email: email}, include: {orders: {include: {item: true}}}})
-        //@ts-ignore
-    const passwordMatches = bcrypt.compareSync(password, user.password)
-    if( user && passwordMatches){
-        res.send({ user , token: createToken(user.id)})
-    }else{
-        throw Error('Boom!')
-    }
-    }catch(err){
-        //@ts-ignore
-        res.status(400).send({error: 'Email/Password invalid!'})
-    }
-    
-})
-
-app.get('/validate', async(req, res) =>{
-const token = request.headers.authorization || ''
- 
-try{
-    const user = await getUserFromToken(token)
-res.send(user)
-}catch(err){
+  try {
+    const hash = bcrypt.hashSync(password);
+    const user = await prisma.user.create({
+      data: { name, email, password: hash },
+    });
+    res.send({ user, token: createToken(user.id) });
+  } catch (err) {
     //@ts-ignore
-    res.status(400).send({error: err.message })
-}
+    res.status(400).send({ error: err.message });
+  }
+});
 
-})
+//SIGN IN
+app.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      include: { orders: { include: { item: true } } },
+    });
+    //@ts-ignore
+    const passwordMatches = bcrypt.compareSync(password, user.password);
+    if (user && passwordMatches) {
+      res.send({ user, token: createToken(user.id) });
+    } else {
+      throw Error("Boom!");
+    }
+  } catch (err) {
+    //@ts-ignore
+    res.status(400).send({ error: "Email/Password invalid!" });
+  }
+});
 
-app.get('/users', async(req, res) =>{
-    const user =  await prisma.user.findMany({include: {orders: true}})
-    res.send(user)
-})
+app.get("/validate", async (req, res) => {
+  const token = request.headers.authorization || "";
 
-app.get('/orders', async(req, res )=>{
-   
-    const order = await prisma.order.findMany({ include: {user : true, item: true}})
-    res.send(order)
-})
+  try {
+    const user = await getUserFromToken(token);
+    res.send(user);
+  } catch (err) {
+    //@ts-ignore
+    res.status(400).send({ error: err.message });
+  }
+});
 
-app.listen(4000, ()=> {
-    console.log(`Server up : http://localhost:4000`)
-})
+app.get("/users", async (req, res) => {
+  const user = await prisma.user.findMany({ include: { orders: true } });
+  res.send(user);
+});
+
+app.get("/orders", async (req, res) => {
+  const order = await prisma.order.findMany({
+    include: { user: true, item: true },
+  });
+  res.send(order);
+});
+
+app.get("users/:email", async (req, res) => {
+  const email = req.params.email;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      include: {
+        orders: {
+          select: { item: true, quantity: true },
+        },
+      },
+    });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ error: "User not found!" });
+    }
+  } catch (err) {
+    //@ts-ignore
+    res.send(400).send(`<pre>${err.message}</pre>`);
+  }
+});
+
+app.listen(4000, () => {
+  console.log(`Server up : http://localhost:4000`);
+});
